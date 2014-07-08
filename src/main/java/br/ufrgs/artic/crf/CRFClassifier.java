21629606108cffc9e3169526f6bc67fbd647a189
alansouza <asouza@hp.com>
2014-07-07 22:33:08 -0700

    Removing unused imports

--- src/main/java/br/ufrgs/artic/crf/CRFClassifier.java
+++ src/main/java/br/ufrgs/artic/crf/CRFClassifier.java
@@ -14,7 +14,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
